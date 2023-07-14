c187263c669409588f3a18d17b9e2db0fe7e0fba
Wes <piuswesley@gmail.com>
2023-07-14 23:50:19 +0800

    Finished user stories 5 and 6

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,25 @@
 import { useState } from 'react';
 import DOMPurify from 'dompurify';
 import {marked} from 'marked';
+import markdownContent from './markdownData';
 import './App.css';
 
+
 function App() {
-  const [editorText, setEditorText] = useState("");
-  // const headerRef = useRef();
+  const [editorText, setEditorText] = useState(markdownContent);
 
   function handleChange(event) {
     setEditorText(event.target.value);
   }
 
-  // headerRef.current.innerHTML = markdown;
-
   const jsx = DOMPurify.sanitize(marked.parse(editorText))
 
   return (
     <div className='container-e'>
-      {/* <div className='divinside'></div> */}
-      <textarea id="editor" className='grow-item' onChange={handleChange} autoFocus placeholder="Put some Github flavor here..."></textarea>
+      <textarea id="editor" className='grow-item' value={editorText} onChange={handleChange} autoFocus placeholder="Put some Github flavor here..."></textarea>
       <div id="preview" className='grow-item' dangerouslySetInnerHTML={{ __html: jsx }}></div>
     </div>
   )
 }
-        
 
 export default App
